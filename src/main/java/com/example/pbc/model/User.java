270d22fc3d4394ed1c82d34bf7db6110cfbe71f0
SpacelessWay <alisa_858@list.ru>
2025-07-07 23:49:43 +0300

    Create User.java

---
+++ src/main/java/com/example/pbc/model/User.java
@@ -0,0 +1,24 @@
+package com.example.pbc.model;
+
+public class User {
+    private Long id;
+    private String login;
+    private String password;
+    public User(Long id, String login, String password){
+        this.id=id;
+        this.login=login;
+        this.password=password;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getLogin(){
+        return login;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+}
