c09cdf017ec331ade0df90a86b5af0b45178bf90
SpacelessWay <alisa_858@list.ru>
2025-07-07 23:50:10 +0300

    Create PasswordEncoder.java

---
+++ src/main/java/com/example/pbc/security/PasswordEncoder.java
@@ -0,0 +1,15 @@
+package com.example.pbc.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public String encode(String rawPassword) {
+        return encoder.encode(rawPassword);
+    }
+
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword, encodedPassword);
+    }
+}
