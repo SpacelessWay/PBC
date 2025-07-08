d4333c253791e8bfbabc08be7b89549902b444cd
SpacelessWay <alisa_858@list.ru>
2025-07-08 23:46:42 +0300

    Update JwtUtil.java

--- src/main/java/com/example/pbc/security/JwtUtil.java
+++ src/main/java/com/example/pbc/security/JwtUtil.java
@@ -3,14 +3,14 @@ package com.example.pbc.security;
 import io.jsonwebtoken.*;
 import org.springframework.stereotype.Component;
 
-import java.util.Base64;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
-//@Component
-public class JwtUtil {
-    String secret = "very-long-secret-key-for-jwt-signing-which-is-over-64-bytes-in-length";
 
-    private final String SECRET_KEY = Base64.getEncoder().encodeToString(secret.getBytes());
+public class JwtUtil {
+
+    private final String secret = "very-long-secret-key-for-jwt-signing-which-is-over-64-bytes-in-length";
+    private final byte[] SIGNING_KEY = secret.getBytes(StandardCharsets.UTF_8);
 
     private final long EXPIRATION = 864_000_000; // 10 дней
 
@@ -18,21 +18,28 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(subject)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS512, SIGNING_KEY)
                 .compact();
     }
 
     public String extractUuid(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+        if (token != null && token.startsWith("Bearer ")) {
+            token= token.substring(7);
+        }
+        return Jwts.parserBuilder()
+                .setSigningKey(SIGNING_KEY)
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
-                .getSubject(); // здесь будет uuid
+                .getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(SIGNING_KEY)
+                    .build()
+                    .parseClaimsJws(token);
             return true;
         } catch (JwtException ex) {
             return false;
